341e6ec89b935ca08d5beadb590e54109c9c5c08
ochuba vincent <ochubiri10@gmail.com>
2023-04-27 13:38:13 -0700

    update

---
+++ tokenizer.c
@@ -0,0 +1,96 @@
+#include "shell.h"
+
+/**
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+
+char **strtow(char *str, char *d)
+{
+	int a, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (a = 0; str[a] != '\0'; a++)
+		if (!is_delim(str[a], d) && (is_delim(str[a + 1], d) || !str[a + 1]))
+			numwords++;
+
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (a = 0, j = 0; j < numwords; j++)
+	{
+		while (is_delim(str[a], d))
+			a++;
+		k = 0;
+		while (!is_delim(str[a + k], d) && str[a + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[a++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
+
+
+/**
+ * **strtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+
+char **strtow2(char *str, char d)
+{
+	int a, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+		if ((str[a] != d && str[a + 1] == d) ||
+				    (str[a] != d && !str[a + 1]) || str[a + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (a = 0, j = 0; j < numwords; j++)
+	{
+		while (str[a] == d && str[a] != d)
+			a++;
+		k = 0;
+		while (str[a + k] != d && str[a + k] && str[a + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[a++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
---
+++ vars.c
@@ -0,0 +1,158 @@
+#include "shell.h"
+
+/**
+ * is_chain - test if current char in buffer is a chain delimeter
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * Return: 1 if chain delimeter, 0 otherwise
+ */
+
+int is_chain(info_t *info, char *buf, size_t *p)
+{
+	size_t b = *p;
+
+	if (buf[b] == '|' && buf[b + 1] == '|')
+	{
+		buf[b] = 0;
+		b++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buf[b] == '&' && buf[b + 1] == '&')
+	{
+		buf[b] = 0;
+		b++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buf[b] == ';') /* found end of this command */
+	{
+		buf[b] = 0; /* replace semicolon with null */
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*p = b;
+	return (1);
+}
+
+
+/**
+ * check_chain - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * @i: starting position in buf
+ * @len: length of buf
+ * Return: Void
+ */
+
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t b = *p;
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			b = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			b = len;
+		}
+	}
+
+	*p = b;
+}
+
+
+/**
+ * replace_alias - replaces an aliases in the tokenized string
+ * @info: the parameter struct
+ * Return: 1 if replaced, 0 otherwise
+ */
+
+int replace_alias(info_t *info)
+{
+	int a;
+	list_t *node;
+	char *p;
+
+	for (a = 0; a < 10; a++)
+	{
+		node = node_starts_with(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = _strchr(node->str, '=');
+		if (!p)
+			return (0);
+		p = _strdup(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
+}
+
+
+/**
+ * replace_vars - replaces vars in the tokenized string
+ * @info: the parameter struct
+ * Return: 1 if replaced, 0 otherwise
+ */
+
+int replace_vars(info_t *info)
+{
+	int a = 0;
+	list_t *node;
+
+	for (a = 0; info->argv[a]; a++)
+	{
+		if (info->argv[a][0] != '$' || !info->argv[a][1])
+			continue;
+
+		if (!_strcmp(info->argv[a], "$?"))
+		{
+			replace_string(&(info->argv[a]),
+					_strdup(convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[a], "$$"))
+		{
+			replace_string(&(info->argv[a]),
+					_strdup(convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		node = node_starts_with(info->env, &info->argv[a][1], '=');
+		if (node)
+		{
+			replace_string(&(info->argv[a]),
+					_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[a], _strdup(""));
+
+	}
+	return (0);
+}
+
+
+/**
+ * replace_string - replaces string
+ * @old: address of old string
+ * @new: new string
+ * Return: 1 if replaced, 0 otherwise
+ */
+
+int replace_string(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
