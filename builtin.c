587954fe56ebc0883c2a2d72d842a02ec9d03396
sapera11 <akinsetewilliams@gmail.com>
2023-05-10 07:13:54 -0700

    update

--- builtin.c
+++ builtin.c
@@ -18,7 +18,7 @@ int _myexit(info_t *info)
 		if (exitcheck == -1)
 		{
 			info->status = 2;
-			print_error(info, "Illegal number: ");
+			print_error(info, "Illegal num: ");
 			_eputs(info->argv[1]);
 			_eputchar('\n');
 			return (1);
